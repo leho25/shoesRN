01ce5377bf81fc9acbcf7aaf309007a61ab3cb0a
Le Ho <leho.tdc2019@gmail.com>
2024-03-06 13:38:05 +0700

    created bottom tabs

--- frontend/src/screens/index.js
+++ frontend/src/screens/index.js
@@ -1,16 +1,56 @@
 import React from 'react';
 import Home from './Home/home';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeDetail from './HomeDetail/homeDelail';
-
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Cart from './Cart/cart';
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+const MainScreen = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, color, size}) => {
+          let iconName;
 
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name ==='Cart'){
+            iconName = focused ? 'cart' : 'cart-outline';
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={Cart}
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 const Index = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="HomeDetail" component={HomeDetail} />
       </Stack.Navigator>
     </NavigationContainer>
